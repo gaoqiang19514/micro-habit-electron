e0ffbddf2e2c437a7142d61298e80c8d6105545f
tomcat <gaoqiang19514@163.com>
2023-12-15 09:50:06 +0800

    feat: 添加倒计时函数

--- renderer.js
+++ renderer.js
@@ -1,6 +1,18 @@
 const startBtn = document.getElementById('btn');
 const musicPlayer = document.getElementById('musicPlayer');
 
+const countDown = (endTime, callback) => {
+  const countdownHelper = () => {
+    const currentTime = new Date().getTime();
+    const secondsRemaining = Math.max(0, Math.floor((endTime - currentTime) / 1000));
+
+    const timer = setTimeout(countdownHelper, 300);
+    callback(secondsRemaining, timer);
+  };
+
+  countdownHelper();
+};
+
 const onFinished = () => {
   musicPlayer.play();
   window.electronAPI.taskFinish({
@@ -13,6 +25,9 @@ startBtn.addEventListener('click', () => {
   // 2. 倒计时结束，调用add创建记录（add接口内部区分是创建还是更新）
   // 3. 显示完成视图
   // 4. 调用onFinished置顶提醒
+
+  const minutes = 10;
+  const endTime = new Date().getTime() + currentTime * 60 * 1000;
 
   setTimeout(() => {
     onFinished();
