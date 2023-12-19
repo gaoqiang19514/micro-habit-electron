22195746515f13d7e96841b80acac66b07042ccf
tomcat <gaoqiang19514@163.com>
2023-12-19 18:17:01 +0800

    chore: 修正less配置

--- craco.config.js
+++ craco.config.js
@@ -1,19 +1,12 @@
 const CracoLessPlugin = require('craco-less');
 
 module.exports = {
-  style: {
-    modules: {
-      localIdentName: '[path][name]__[local]--[hash:base64:5]',
-    },
-  },
   plugins: [
     {
       plugin: CracoLessPlugin,
       options: {
-        lessLoaderOptions: {
-          lessOptions: {
-            javascriptEnabled: true,
-          },
+        cssLoaderOptions: {
+          modules: { localIdentName: '[local]_[hash:base64:5]' },
         },
       },
     },
